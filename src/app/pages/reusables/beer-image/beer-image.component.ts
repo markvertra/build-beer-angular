47048701f6dcc13008a3fe3ea6c93cc8c8e08458
Mark Allen <mark@traversify.com>
2017-10-02 20:27:33 +0200

    Made canvas a bottle

--- src/app/pages/reusables/beer-image/beer-image.component.ts
+++ src/app/pages/reusables/beer-image/beer-image.component.ts
@@ -23,10 +23,20 @@ export class BeerImageComponent implements AfterViewInit {
     const ctx: CanvasRenderingContext2D = this.myCanvas.nativeElement.getContext('2d');
     ctx.fillStyle = this.color;
     ctx.beginPath();
-    ctx.moveTo(0, 50);
-    ctx.lineTo(50, 500);
-    ctx.lineTo(250, 500);
-    ctx.lineTo(300, 50);
+    ctx.moveTo(150, 25);
+    ctx.quadraticCurveTo(143, 30, 150, 35);
+    ctx.lineTo(150, 40);
+    ctx.quadraticCurveTo(140, 45, 150, 50);
+    ctx.lineTo(130, 150);
+    ctx.quadraticCurveTo(110, 175, 100, 200);
+    ctx.lineTo(100, 500);
+    ctx.quadraticCurveTo(175, 530, 250, 500);
+    ctx.lineTo(250, 200);
+    ctx.quadraticCurveTo(240, 175, 220, 150);
+    ctx.lineTo(200, 50);
+    ctx.quadraticCurveTo(210, 45, 200, 40);
+    ctx.lineTo(200, 35);
+    ctx.quadraticCurveTo(207, 30, 200, 25);
     ctx.fill();
   }
 

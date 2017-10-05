0ed61e362974860e3dac19f2272f0be43c0fe1a4
Mark Allen <mark@traversify.com>
2017-10-05 12:05:16 +0200

    login check working

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,8 +1,6 @@
 export class User {
   id: string;
-  password: string;
   username: string;
-
 
   constructor(obj: Object = {}) {
     Object.assign(this, obj);
--- src/app/services/session-service.service.ts
+++ src/app/services/session-service.service.ts
@@ -5,16 +5,25 @@ import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
+import { User } from '../models/user';
+
+
 @Injectable()
 export class SessionService {
+
   private initialized: boolean;
-  private user;
-  private userChange: Subject<null> = new Subject();
+  private user: User;
+  private userChange: Subject<User | null> = new Subject();
 
   userChange$ = this.userChange.asObservable();
 
   BASE_URL = 'http://localhost:3000';
   constructor(private http: Http) { }
+
+  private setUser(user: User = null) {
+    this.user = user;
+    this.userChange.next(user);
+  }
 
   handleError(e) {
     return Observable.throw(e.json().message);
@@ -39,14 +48,24 @@ export class SessionService {
   }
 
   isLoggedIn() {
-    return this.http.get(`${this.BASE_URL}/auth/loggedin`)
-      .map(res => res.json())
-      .catch(this.handleError);
+    const options = new RequestOptions();
+    options.withCredentials = true;
+    return this.http.get(`${this.BASE_URL}/auth/loggedin`, options)
+      .map(res => {
+        const user = new User(res.json());
+        this.setUser(user);
+        return user;
+      }, (err) => {
+        if ( err.status === 404) {
+          this.setUser();
+        }
+      });
   }
 
-  getPrivateData() {
-    return this.http.get(`${this.BASE_URL}/auth/private`)
-      .map(res => res.json())
-      .catch(this.handleError);
+  initialize() {
+    if (!this.initialized) {
+      this.initialized = true;
+      this.isLoggedIn().subscribe();
+    }
   }
 }

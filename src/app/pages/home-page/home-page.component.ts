9bb39af4990761d23fdf88e58edd5be8d6d88261
Mark Allen <mark@traversify.com>
2017-10-06 14:57:14 +0200

    User name links to page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BeerService } from '../../services/beer-service.service';
+import { SessionService } from '../../services/session-service.service';
+
 
 @Component({
   selector: 'app-home-page',
@@ -9,12 +11,23 @@ import { BeerService } from '../../services/beer-service.service';
 })
 export class HomePageComponent implements OnInit {
   beers;
+  user;
 
+  constructor(private beer: BeerService,
+              private router: Router,
+              private session: SessionService) { }
 
-  constructor(private beer: BeerService, private router: Router) { }
 
   ngOnInit() {
     this.beer.getPublicBeers().subscribe((res) => this.beers = res);
+    this.session.isLoggedIn()
+    .subscribe(
+      (user) => { this.setUser(user); }
+    );
+  }
+
+  setUser(user: any | null) {
+    this.user = user;
   }
 
   handleBeerClick(id: String) {

e25306c43cf65a1d64670db0a3bc907f9dadb9f2
Mark Allen <mark@traversify.com>
2017-10-07 15:43:09 +0200

    added search functionality

--- src/app/pages/home-page/components/beer-display/beer-display.component.ts
+++ src/app/pages/home-page/components/beer-display/beer-display.component.ts
@@ -9,16 +9,35 @@ import { SessionService } from '../../../../services/session-service.service';
   styleUrls: ['./beer-display.component.css']
 })
 export class BeerDisplayComponent implements OnInit {
-  beers;
-  user;
-  searcher;
+  beers: any;
+  user: any;
+  searchTerm: string;
+  searchType: string;
+  filteredBeers: any;
 
   constructor(private beer: BeerService,
     private router: Router,
     private session: SessionService) { }
 
   ngOnInit() {
-    this.beer.getPublicBeers().subscribe((res) => this.beers = res);
+    this.beer.getPublicBeers().subscribe((res) => (this.filteredBeers = res, this.beers = res));
+  }
+
+  handleSearchChange() {
+    this.filteredBeers = this.runSearch(this.searchTerm, this.searchType);
+  }
+
+  handleSearchTypeChange() {
+    this.filteredBeers = this.runSearch(this.searchTerm, this.searchType);
+  }
+
+  runSearch(searchTerm, searchType) {
+    if (!searchType) { searchType = 'searchable'; }
+    if (!searchTerm) { searchTerm = ''; }
+    console.log(this.filteredBeers[3]['searchable']);
+    return this.beers.filter((beer) => {
+      return beer[searchType].toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+    });
   }
 
   handleBeerClick(id: String) {

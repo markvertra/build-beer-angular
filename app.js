391d61a3df25b9ee89b5c2ebc45fba98f9b683d4
Mark Allen <mark@traversify.com>
2017-10-11 15:28:54 +0200

    Heroku ng serve bug attempt fix

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist'));
+
+// ONLY FUNCTIONS ON HEROKU
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   };
+// };
+
+// app.use(forceSSL());
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+ });
+
+app.listen(process.env.PORT || 8080);

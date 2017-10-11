e0fd1df1e50a317a06c398b0985c0a4ee726a0cc
Mark Allen <mark@traversify.com>
2017-10-11 16:17:12 +0200

    redirect http to https

--- app.js
+++ app.js
@@ -5,19 +5,18 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-// ONLY FUNCTIONS ON HEROKU
-// const forceSSL = function() {
-//   return function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(
-//        ['https://', req.get('Host'), req.url].join('')
-//       );
-//     }
-//     next();
-//   };
-// };
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  };
+};
 
-// app.use(forceSSL());
+app.use(forceSSL());
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));

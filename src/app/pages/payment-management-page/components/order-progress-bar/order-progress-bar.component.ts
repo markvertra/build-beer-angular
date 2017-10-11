56f3cfaf4c14971be62fa5d9bf6d151aa7805bc8
Mark Allen <mark@traversify.com>
2017-10-11 18:01:37 +0200

    order progress bar working

--- src/app/pages/payment-management-page/components/order-progress-bar/order-progress-bar.component.ts
+++ src/app/pages/payment-management-page/components/order-progress-bar/order-progress-bar.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-order-progress-bar',
   templateUrl: './order-progress-bar.component.html',
   styleUrls: ['./order-progress-bar.component.css']
 })
-export class OrderProgressBarComponent implements OnInit {
+export class OrderProgressBarComponent implements OnChanges {
   @Input () status: string;
   basket;
   confirm;
@@ -14,7 +14,7 @@ export class OrderProgressBarComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
     this.progressChanger();
   }
 
